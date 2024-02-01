b7c8c8863a7922598a43158e84ee2771a65bbf44
Lucimara Ribeiro <mara.ribeirojm@gmail.com>
2024-02-01 10:51:27 -0300

    adicionado arquivos iniciais

---
+++ Desafio_1.js
@@ -0,0 +1,24 @@
+numeroUm = 1
+stringUm = '1'
+numeroTrinta = 30
+stringTrinta = '30'
+numeroDez = 10
+stringDez = '10'
+
+if (numeroUm == stringUm) {
+    console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor');
+}
+
+if (numeroTrinta == stringTrinta) {
+    console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor');
+}
+
+if (numeroDez == stringDez) {
+    console.log('As variáveis numeroUm e stringUm têm o mesmo valor, mas tipos diferentes');
+} else {
+    console.log('As variáveis numeroUm e stringUm não têm o mesmo valor');
+}
